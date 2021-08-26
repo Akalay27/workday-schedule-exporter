65a08411152066ccb3fed2ec02fa382bcc4df5b7
KaiNakamura <kaihnakamura@gmail.com>
2021-08-25 20:17:35 -0400

    Remove unused code, add authors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import Converter from './util/Converter';
-import FileInputDragDrop from './components/FileInputDragDrop'
 import FileInputButton from './components/FileInputButton'
 import Step from './util/Step'
 import './App.css';
@@ -16,9 +15,6 @@ function App() {
 
 	return (
 		<div className="App">
-			<FileInputDragDrop handleFile={(file) => uploadFile(file)}>
-				<p>Drop to Upload</p>
-			</FileInputDragDrop>
 			<header className="App-header">
 				<h1>Workday Schedule Exporter</h1>
 				<p>Upload an .XLSX or .CSV spreadsheet and convert it to an .ICS</p>
@@ -29,6 +25,10 @@ function App() {
 			</FileInputButton>
 
 			<p>{step}</p>
+
+			<footer className="App-footer">
+				<p>Made by Adam Kalayjian and Kai Nakamura</p>
+			</footer>
 		</div>
 	);
 }
